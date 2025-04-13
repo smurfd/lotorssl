529173de7c27920ea50fe27f73b6c6620377c3cf
Nicklas Boman <smurfd@gmail.com>
2025-04-13 22:25:26 +0200

    c backend, blehrg, lets do the rosetta example with arrays, mod

--- lotorssl/src/rewrite/lotorssl/src/ec.c
+++ lotorssl/src/rewrite/lotorssl/src/ec.c
@@ -1,5 +1,6 @@
 // Auth: smurfd, 2025 More reading at the bottom of the file; 2 spacs indent; 150 width                                                             //
 // ------------------------------------------------------------------------------------------------------------------------------------------------ //
+#include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -36,42 +37,65 @@ static inline uint32_t checkbit(const uint32_t *a, const int16_t b) {
 }
 
 // Math
-// return mod(a, N)
-static inline void mod_n(uint32_t *r, const uint32_t *a, const struct curve_t *c) {
-  uint64_t t[8] = {0};
-  for (uint8_t i = 0; i < 8; i++) {
-    t[i] = a[i] % c->n[i];
-    if (t[i] < 0) printf("tn small\n");
-    r[i] = t[i];
+// return mod(a, n)
+static inline void mod_n(uint32_t *r, const uint32_t *a, const curve *c) {
+  uint32_t t0 = a[0], t1 = a[1], t2 = a[2], t3 = a[3], t4 = a[4], t5 = a[5], t6 = a[6], t7 = a[7], r0 = 0;
+  r0 = t0 > c->n[0];
+  t0 %= c->n[0];
+  if (r0) {
+    t0 -= 1;
+    t1 = (u64)(t1 + 0xffffffff % c->n[1]);
+    t2 = (u64)(t2 + 0xffffffff % c->n[2]);
+    t3 = (u64)(t3 + 0xffffffff % c->n[3]);
+    t4 = (u64)(t4 + 0xffffffff % c->n[4]);
+    t5 = (u64)(t5 + 0xffffffff % c->n[5]);
+    t6 = (u64)(t6 + 0xffffffff % c->n[6]);
+    t7 = (u64)(t7 + 0xffffffff % c->n[7]);
+    t7 += 1;
   }
+  r[0] = t0; r[1] = t1; r[2] = t2; r[3] = t3; r[4] = t4; r[5] = t5; r[6] = t6; r[7] = t7;
 }
 
 // return mod(a, p)
-static inline void mod_p(uint32_t *r, const uint32_t *a, const struct curve_t *c) {
-  uint64_t t[8] = {0};
-  for (uint8_t i = 0; i < 8; i++) {
-    t[i] = a[i] % c->p[i];
-    if (t[i] < 0) printf("tr small\n");
-    r[i] = t[i];
+static inline void mod_p(uint32_t *r, const uint32_t *a, const curve *c) {
+  uint32_t t0 = a[0], t1 = a[1], t2 = a[2], t3 = a[3], t4 = a[4], t5 = a[5], t6 = a[6], t7 = a[7], r0 = 0;
+  r0 = t0 > c->p[0];
+  t0 %= c->p[0];
+  if (r0) {
+    t0 -= 1;
+    t1 = (u64)(t1 + 0xffffffff % c->p[1]);
+    t2 = (u64)(t2 + 0xffffffff % c->p[2]);
+    t3 = (u64)(t3 + 0xffffffff % c->p[3]);
+    t4 = (u64)(t4 + 0xffffffff % c->p[4]);
+    t5 = (u64)(t5 + 0xffffffff % c->p[5]);
+    t6 = (u64)(t6 + 0xffffffff % c->p[6]);
+    t7 = (u64)(t7 + 0xffffffff % c->p[7]);
+    t7 += 1;
   }
+  r[0] = t0; r[1] = t1; r[2] = t2; r[3] = t3; r[4] = t4; r[5] = t5; r[6] = t6; r[7] = t7;
 }
 
 // Public
-struct curve_t *getcurve(void) {
+curve *getcurve(void) {
   return &c;
 }
 
 // Public debug
-void debug_printcurve(const struct curve_t *c) {
+void debug_printcurve(const curve *c) {
   printuint32("a ", c->a);
   printuint32("b ", c->b);
   printuint32("n ", c->n);
   printuint32("p ", c->p);
   printuint32("Gx", c->G);
   printuint32("Gy", c->G+8);
-  uint32_t r[8] = {0};
-  mod_p(r, c->b, c);
-  printuint32("mod_p", r);
-  mod_n(r, c->G, c);
-  printuint32("mod_n", r);
+  uint32_t r1[8] = {0}, correctn[8] = {0}, r2[8] = {0}, correctp[8] = {0};
+  mod_n(r1, c->G, c);
+  mod_p(r2, c->G, c);
+  str2uint32(correctn, "26c1d6569a2022ce7e85889d47ed1ff4ce870b0855a06295f9dcbbac79be667f");
+  str2uint32(correctp, "f6f8179859f2815b2dce28d9029bfcdbce870b0755a06295f9dcbbac79be667e");
+  printuint32("mod_n", r1);
+  printuint32("corkn", correctn);
+  printuint32("mod_p", r2);
+  printuint32("corkp", correctp);
 }
+
--- lotorssl/src/rewrite/lotorssl/src/tests/testec.c
+++ lotorssl/src/rewrite/lotorssl/src/tests/testec.c
@@ -7,12 +7,12 @@
 
 int main(void) {
   uint32_t private[8] = {0}, public[16] = {0}, hash[8] = {0}, sig[16] = {0}; // ?
-  const struct curve_t *c = getcurve();
+  const curve *c = getcurve();
   // make_keypair(public, private, c);
   // memcpy(hash, public, sizeof(hash));
   // sign(private, hash, sizeof(hash), sig, c);
   // verify(public, hash, sizeof(hash), sig, c);
   debug_printcurve(c);
   printf("OK\n");
-
 }
+
