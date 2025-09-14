cfb3e790cf6a1b8a774d93ec9435a88d39294b9b
Nicklas Boman <smurfd@gmail.com>
2025-09-14 21:58:11 +0200

    lotorssl, start adding server client connections

---
+++ src/crpt.c
@@ -0,0 +1,235 @@
+// Auth: smurfd, 2025 More reading & Borrow/Stolen parts read at the bottom of the file; 2 spacs indent; 150 width                                  //
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include "bmec.h"
+#include "crpt.h"
+
+//
+// Communication init
+sockets communication_init(const char *host, const char *port) {
+  sockets sock;
+  sock.descriptor = socket(AF_INET , SOCK_STREAM , 0);
+  if (sock.descriptor == -1) {
+    perror("\"[o.o]\" \t Could not create a socket\n");
+    exit(0);
+  }
+  memset(&(sock.addr), '\0', sizeof(sock.addr));
+  sock.addr.sin_family = AF_INET;
+  sock.addr.sin_port = atoi(port);
+  sock.addr.sin_addr.s_addr = inet_addr(host);
+  return sock;
+}
+
+//
+// Connection init
+connection connection_init(int8_t descriptor, int type) {
+  connection c;
+  c.socket = descriptor;
+  c.type = type;
+  if (descriptor >= 0) c.err = 0;
+  else c.err = -1;
+  return c;
+}
+
+//
+// Send public key
+void send_publickey(connection c, bint *pubx, bint *puby) {
+  int sock = *((connection*)&c)->clisocket;
+  send(sock, pubx, sizeof(bint), 0);
+  send(sock, puby, sizeof(bint), 0);
+}
+
+//
+// Receive public key
+void receive_publickey(connection c, bint *pubx, bint *puby) {
+  int sock = *((connection*)&c)->clisocket;
+  recv(sock, pubx, sizeof(bint), 0);
+  recv(sock, puby, sizeof(bint), 0);
+}
+
+//
+// Send encrypted data
+void send_encrypteddata(connection c, encd e) {
+  int sock = *((connection*)&c)->clisocket;
+  send(sock, &e, sizeof(encd), 0);
+}
+
+//
+// Receive encrypted data
+void receive_encrypteddata(connection c, encd e) {
+  int sock = *((connection*)&c)->clisocket;
+  recv(sock, &e, sizeof(encd), 0);
+}
+
+static void *server_connection_handler_ssl(void *conn) { // TODO: use template from examples
+  if (((connection*)conn)->socket == -1) return (void*) - 1;
+  return 0;
+}
+
+static void *server_connection_handler(void *conn) {
+  bint alsk = bcreate(), alpkx = bcreate(), alpky = bcreate(), bopkx = bcreate(), bopky = bcreate();
+  bint alshrx = bcreate(), alshry = bcreate(), boshrx = bcreate(), boshry = bcreate();
+  if (((connection*)conn)->socket == -1) return (void*) - 1;                    // Handshake vvv
+  // generate server keypair
+  genkeypair(&alpkx, &alpky, &alsk); // Alice's keypair generated
+  // send server public key to client
+  send_publickey(*(connection*)conn, &alpkx, &alpky);
+  // receive public key from client
+  receive_publickey(*(connection*)conn, &bopkx, &bopky);
+  // generate shared secret
+  gensharedsecret(&alshrx, &alshry, &alsk, &alpkx, &alpky); // Alice's shared secret(but with alices private key)
+  gensharedsecret(&boshrx, &boshry, &alsk, &bopkx, &bopky); // Bob's shared secret
+  // verify that the shared secrets are the same
+  verifysharedsecret(&alshrx, &alshry, &boshrx, &boshry, &alsk, &alsk); // Verify Alice's and Bob's shared secrets
+  // TODO: use shared secret to encrypt
+  // send_encrypteddata();
+  // if (receive_encrypteddata() > 0) {                                           // Handshake ^^^
+    // TODO: verify that the encrypted data is ok
+    pthread_t ssl_thread;
+    if (pthread_create(&ssl_thread, NULL, server_connection_handler_ssl, (void*)conn) < 0) {
+      perror("\"[o.o]\" \t Could not create thread");
+    }
+    pthread_join(ssl_thread, NULL);
+  // }
+
+  // Alice and Bob, agree on a large prime number p and a generator g, where g is a primitive root of p.
+  // Both p and g are public knowledge and can be shared openly.
+  // Alice chooses a random secret number, a, and calculates A = g^a mod p. Sendd A to Bob
+  // Bob chooses a random secret number, b, and calculates B = g^b mod p. Send B to Alice
+  // Alice calculates the shared secret key as K = B^a mod p.
+  // Bob calculates the shared secret key as K = A^b mod p.
+
+  // Both parties now have the same secret key, K, which can be used for symmetric encryption and decryption
+  // of messages exchanged over the insecure channel. Since private keys a and b are never shared,
+  // an eavesdropper who intercepts the messages between Alice and Bob cannot calculate the secret key.
+  return 0;
+}
+
+static int server_run(const char *host, const char *port) {
+  sockets sock = communication_init(host, port);
+  if (bind(sock.descriptor, (struct sockaddr*)&(sock.addr), sizeof(sock.addr)) < 0) {
+    perror("\"[o.o]\" \t Bind error");
+    exit(0);
+  }
+  listen(sock.descriptor, 3);
+  return sock.descriptor;
+}
+
+int server_handle(connection conn) {
+  int client_sock, c = sizeof(struct sockaddr_in);
+  struct sockaddr_in client;
+  while ((client_sock = accept(conn.socket, (struct sockaddr*)&client, (socklen_t*)&c))) {
+    pthread_t thread;
+    conn.clisocket = &client_sock;
+    if (pthread_create(&thread, NULL, server_connection_handler, (void*)&conn) < 0) {
+      perror("\"[o.o]\" \t Could not create thread");
+      return 1;
+    }
+    pthread_join(thread, NULL);
+  }
+  if (client_sock < 0) {
+    perror("\"[o.o]\" \t No clients connected");
+    return 1;
+  }
+  return client_sock;
+}
+
+//
+// Initialize server
+connection server_init(const char *host, const char *port, int type) {
+  int socket_desc = server_run(host, port);
+  printf("\"[o.o]\" \t eating food...\n");
+  return connection_init(socket_desc, type);
+}
+
+//
+// End connection
+void server_end(connection c) {
+  close(c.socket);
+}
+
+static void *client_connection_handler_ssl(void *conn) { // TODO: use template from examples
+  if (((connection*)conn)->socket == -1) return (void*) - 1;
+  return 0;
+}
+
+static void *client_connection_handler(void *conn) {
+  bint alsk = bcreate(), alpkx = bcreate(), alpky = bcreate(), bopkx = bcreate(), bopky = bcreate();
+  bint alshrx = bcreate(), alshry = bcreate(), boshrx = bcreate(), boshry = bcreate();
+  if (((connection*)conn)->socket == -1) return (void*) - 1;                    // Handshake vvv
+  // generate client keypair
+  genkeypair(&alpkx, &alpky, &alsk); // Alice's keypair generated
+  // receive server public key from server
+  receive_publickey(*(connection*)conn, &bopkx, &bopky);
+  // send public key to server
+  send_publickey(*(connection*)conn, &alpkx, &alpky);
+  // generate shared secret
+  gensharedsecret(&alshrx, &alshry, &alsk, &alpkx, &alpky); // Alice's shared secret(but with alices private key)
+  gensharedsecret(&boshrx, &boshry, &alsk, &bopkx, &bopky); // Bob's shared secret
+  // verify that the shared secrets are the same
+  verifysharedsecret(&alshrx, &alshry, &boshrx, &boshry, &alsk, &alsk); // Verify Alice's and Bob's shared secrets
+  // TODO: use shared secret to encrypt using AES
+  // receive_encrypteddata();
+  // if (send_encrypteddata() > 0) {                                        // Handshake ^^^ 
+    // TODO: verify that the encrypted data is ok
+    pthread_t ssl_thread;
+    if (pthread_create(&ssl_thread, NULL, client_connection_handler_ssl, (void*)conn) < 0) {
+      perror("\"[o.o]\" \t Could not create thread");
+    }
+    pthread_join(ssl_thread, NULL);
+  // }
+
+  // Alice and Bob, agree on a large prime number p and a generator g, where g is a primitive root of p.
+  // Both p and g are public knowledge and can be shared openly.
+  // Alice chooses a random secret number, a, and calculates A = g^a mod p. Sendd A to Bob
+  // Bob chooses a random secret number, b, and calculates B = g^b mod p. Send B to Alice
+  // Alice calculates the shared secret key as K = B^a mod p.
+  // Bob calculates the shared secret key as K = A^b mod p.
+
+  // Both parties now have the same secret key, K, which can be used for symmetric encryption and decryption
+  // of messages exchanged over the insecure channel. Since private keys a and b are never shared,
+  // an eavesdropper who intercepts the messages between Alice and Bob cannot calculate the secret key.
+  return 0;
+}
+
+static int client_run(const char *host, const char *port) {
+  sockets sock = communication_init(host, port);
+  if (connect(sock.descriptor , (struct sockaddr*)&(sock.addr) , sizeof(sock.addr)) < 0) {
+    perror("\"[o.o]\" \t Connection error");
+    return 1;
+  }
+  return sock.descriptor;
+}
+
+int client_handle(connection conn) {
+  pthread_t thread;
+  conn.clisocket = &(conn.socket);
+  if (pthread_create(&thread, NULL, client_connection_handler, (void*)&conn) < 0) {
+    perror("\"[o.o]\" \t Could not create a thread");
+    return 1;
+  }
+  pthread_join(thread, NULL);
+  return conn.socket;
+}
+
+//
+// Initialize client
+connection client_init(const char *host, const char *port, int type) {
+  printf("\"[o.o]\" \t finding food...\n");
+  int socket_desc = client_run(host, port);
+  return connection_init(socket_desc, type);
+}
+
+//
+// End connection
+void client_end(connection c) {
+  close(c.socket);
+}
+
+// Very simple handshake
---
+++ src/crpt.h
@@ -0,0 +1,95 @@
+// Auth: smurfd, 2023 More reading & Borrow/Stolen parts read at the bottom of the file; 2 spacs indent; 120 width    //
+#ifndef CRYPTO_H
+#define CRYPTO_H 1
+#include <stdbool.h>
+#include <stdint.h>
+#include <netinet/in.h>
+#include "bmec.h"
+
+#define u64 unsigned long long int // because linux uint64_t is not same as on mac
+#define BLOCK 1024
+
+typedef struct header {
+  u64 len;                   // length
+  u64 ver;                   // version
+  u64 g;                     // global
+  u64 p;                     // private
+} head;
+
+typedef struct cryptokeys {
+  u64 publ;                  // public key
+  u64 priv;                  // private key
+  u64 shar;                  // shared key
+} cryptokey;
+
+typedef struct connection {
+  int socket;                // socket used for connection
+  int *clisocket;            // clientsocket
+  uint8_t type;              // what type of client/server: 1 = keyvaluestore, 2 = tablesdb
+  uint8_t err;               // error
+} connection;
+
+typedef struct sockets {
+  int8_t descriptor;         // socket descriptor
+  struct sockaddr_in addr;   // socket addr
+} sockets;
+
+typedef struct encd {
+  bint data[1024];
+} encd;
+
+connection connection_init(int8_t descriptor, int type);
+sockets communication_init(const char *host, const char *port);
+
+// Send/Receive
+//int send_cryptodata(connection c, void* data, head *h, u64 len);
+//void send_cryptokey(connection c, head *h, cryptokey *k);
+//int receive_cryptodata(connection c, void* data, head *h, u64 len);
+//void receive_cryptokey(connection c, head *h, cryptokey *k);
+
+void send_publickey(connection c, bint *pubx, bint *puby);
+void receive_publickey(connection c, bint *pubx, bint *puby);
+void send_encrypteddata(connection c, encd e);
+void receive_encrypteddata(connection c, encd e);
+
+// Generators
+//cryptokey generate_cryptokeys(head *h);
+void handler_cryptography(u64 data, cryptokey k, u64 *enc);
+
+// Tooling
+//int usage(char *arg, int count, char *clisrv);
+#endif
+
+// Very simple handshake
+/*
+```
+    |                                                     |                    .
+ cli|                                                     |srv                 .
+    |                                                     |                    .
+                                                                               .
+     _____________ [1] TCP HANDSHAKE _____________________                     .
+                                                                               |
+     ----- >>> --- [1.1] syn ------------------- >   ----v                     |
+     v---- <   --- [1.2] syn ack --------------- <<< -----        handled by os|
+     ----- >>> --- [1.3] ack ------------------- >   -----                     |
+                              v                                                |
+                                                                               .
+     _____________ [2] TLS HANDSHAKE _____________________                     .
+                                                                               .
+     ----- >>> --- [2.1] client hi ------------- >   ----v                     .
+     ----- <   --- [2.1] server hi ------------- <<< -----                     .
+     v---- <   --- [2.2] verify server crt ----- <<< -----                     .
+     ----- >>> --- [2.3] client crt ------------ >   -----                     .
+     ----- >>> --- [2.4] key exchange ---------- >   -----                     .
+     ----- >>> --- [2.5] change cipher spec ---- >   -----                     .
+     ----- >>> --- [2.6] client finish --------- >   ----v                     .
+     ----- <   --- [2.7] change cipher spec ---- <<< -----                     .
+     v---- <   --- [2.8] server finished ------- <<< -----                     .
+     =-=-= >>> -=- [2.9] encrypted traffic -=-=- <<< -=-=-                     .
+                                                                               .
+     ...                                                                       .
+     ...                                                                       .
+     ...                                                                       .
+     ...                                                                       .
+```
+*/
